6adbd96ca1e38890eb3d45d2711740ced8a82412
adeboladee <adebolashode@gmail.com>
2023-10-31 11:22:24 +0100

    A quiz App
    Loads five different questions and shows result

--- src/App.js
+++ src/App.js
@@ -1,75 +1,88 @@
-// import Start from "./Components/Start";
 import Questions from "./Components/Questions";
 import React from "react";
+// import Confetti from "react-confetti";
 
 export default function App() {
 	const [game, setGame] = React.useState({
 		hasStarted: false,
 	});
 
+	const [count, setCount] = React.useState(0);
+
+	const [allQuestions, setAllQuestions] = React.useState([]);
+
+	const [selectedAnswer, setSelectedAnswer] = React.useState({});
+
+	const [quizEnded, setQuizEnded] = React.useState(false);
+
+	const [result, setResult] = React.useState(0);
+
 	function startGame() {
 		setGame((prevGame) => ({ hasStarted: !prevGame.hasStarted }));
 	}
 
-	const [allQuestions, setAllQuestions] = React.useState([]);
-
-	// const [selected, setSelected] = React.useState(false);
-
-	// const selectAnswer = () => {
-	// 	setSelected((prevSelect) => !prevSelect);
-	// };
-
 	React.useEffect(() => {
-		fetch("https://opentdb.com/api.php?amount=5")
+		fetch(
+			"https://opentdb.com/api.php?amount=5&category=21&difficulty=easy"
+		)
 			.then((res) => res.json())
 			.then((data) => setAllQuestions(data.results));
-	}, []);
+	}, [count]);
 
-	const [selectedAnswers, setSelectedAnswers] = React.useState([]);
-
-	const [quizEnded, setQuizEnded] = React.useState(false);
-	const [result, setResult] = React.useState(0);
-
-	const clickHandleSelect = (selectedItem, choice) => {
-		setSelectedAnswers((prev) => [
+	const clickHandleSelect = (qIndex, choice) => {
+		setSelectedAnswer((prev) => ({
 			...prev,
-			selectedItem === choice ? "correct" : "incorrect",
-		]);
-		console.log(selectedItem, choice);
+			[qIndex]: prev[qIndex] === choice ? "" : choice,
+		}));
 	};
 
-	console.log(selectedAnswers);
-
-	const question = allQuestions.map((item) => {
+	const question = allQuestions.map((item, i) => {
 		return (
 			<Questions
+				key={i}
+				questionIndex={i}
 				item={item}
-				handleSelect={(item, choice) => clickHandleSelect(item, choice)}
+				handleSelect={(qIndex, choice) =>
+					clickHandleSelect(qIndex, choice)
+				}
+				quizEnded={quizEnded}
+				selected={selectedAnswer[i]}
 			/>
 		);
 	});
 
-	// React.useEffect(() => {
-	// 	if (quizEnded === true) {
-	// 		let score = 0;
+	React.useEffect(() => {
+		if (quizEnded === true) {
+			let score = 0;
 
-	// 		});
-	// 		setResult(score);
-	// 	}
-	// }, [quizEnded]);
+			allQuestions.map((question, index) => {
+				score =
+					question.correct_answer == selectedAnswer[index]
+						? score + 1
+						: score;
+			});
+			setResult(score);
+		}
+	}, [quizEnded]);
 
-	// console.log(allQuestions);
+	const playAgain = () => {
+		setCount((count) => count + 1);
+		setQuizEnded(false);
+	};
 
 	return (
 		<div>
-			{JSON.stringify(selectedAnswers, null, 2)}
+			{/* {quizEnded && result == 5 && <Confetti />} */}
+			{/* {JSON.stringify(selectedAnswer, null, 2)} */}
 			{game.hasStarted ? (
 				<section>
 					{question}
 					{quizEnded === true ? (
 						<div className="result-wrapper">
 							<span>You scored {result}/5 correct answers</span>
-							<button className="new-game">Play Again</button>
+							<button className="new-game" onClick={playAgain}>
+								Play Again
+							</button>
 						</div>
 					) : (
 						<button
--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,37 +1,35 @@
-import { useState } from "react";
-
 export default function Questions(props) {
 	const answersList = [
 		...props.item.incorrect_answers,
 		props.item.correct_answer,
 	].sort((a, b) => (a > b ? 1 : -1));
 
-	const [selectedIndex, setSelectedIndex] = useState(-1);
+	const handleOptions = answersList.map((item, i) => {
+		let styles;
+		if (props.quizEnded === false) {
+			styles = props.selected === item ? "selected" : "question-btn";
+		} else if (props.quizEnded === true) {
+			styles =
+				item === props.item.correct_answer ? "correct" : "question-btn";
+			if (props.selected === item && props.item.correct_answer !== item) {
+				styles = " wrong";
+			}
+		}
 
-	// const styles = {
-	// 	backgroundColor: item ? "#59E391" : "white",
-	// };
+		return (
+			<button
+				className={styles}
+				key={`${item}_${i}`}
+				value={item}
+				onClick={() => {
+					props.handleSelect(props.questionIndex, item);
+				}}
+			>
+				{item}
+			</button>
+		);
+	});
 
-	console.log(props.item.correct_answer);
-
-	const handleOptions = answersList.map((item, i) => (
-		<button
-			className={selectedIndex === i ? "selected" : "question-btn"}
-			// style={styles}
-			key={`${item}_${i}`}
-			onClick={() => {
-				props.handleSelect(item, props.item.correct_answer);
-				setSelectedIndex(i);
-				// console.log(i);
-			}}
-		>
-			{item}
-		</button>
-	));
-
-	// const handleSelect = () => {
-	// 	console.log(item);
-	// };
 	return (
 		<main>
 			<p className="question">{props.item.question}</p>
